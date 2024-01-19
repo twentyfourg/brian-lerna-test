d710eaf0e93c3e05f215b43760aac20cc60fca05
Brian Anstett <brian.anstett@24g.com>
2024-01-19 15:33:39 -0500

    feat(action2): initialize action2 package

---
+++ packages/action2/README.md
@@ -0,0 +1,11 @@
+# `action2`
+
+> TODO: description
+
+## Usage
+
+```
+const action2 = require('action2');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/action2/__tests__/action2.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const action2 = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(action2(), 'Hello from action2');
+console.info('action2 tests passed');
